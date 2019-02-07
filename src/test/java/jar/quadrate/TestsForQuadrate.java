6cba4c23c9cbc694a872fbf9781796128d925ba7
Maria Kuznetsova <maria_kuznetsova1@epam.com>
2019-02-07 09:08:46 +0300

    fvvf

--- src/test/java/jar/quadrate/TestsForQuadrate.java
+++ src/test/java/jar/quadrate/TestsForQuadrate.java
@@ -1,5 +1,6 @@
 package jar.quadrate;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -10,14 +11,14 @@ public class TestsForQuadrate extends Parent {
 
 	@Parameters("sideOfASquare")
 	@Test(description = "Тестируется возможность создания объекта класса Quadrate", groups = "testGetQuadrate")
-	public void testGetQuadrate(double sideOfASquareFromXML) {
+	public void testGetQuadrate(@Optional ("") double sideOfASquareFromXML) {
 		Object quadrate = getGeomObject().getQuadrate(sideOfASquareFromXML);
 		assertEquals(quadrate.getClass(), Quadrate.class);
 	}
 
 	@Parameters("sideOfASquare")
 	@Test(groups = "testGetPerimeter", dependsOnGroups = "testGetQuadrate", description="Тестируется правильность вычисления переметра")
-	public void testGetPerimeter(double sideOfASquareFromXML) {
+	public void testGetPerimeter(@Optional ("") double sideOfASquareFromXML) {
 		double myPerimeter = 4 * sideOfASquareFromXML;
 		double testPerimeter = getGeomObject().getQuadrate(sideOfASquareFromXML).getPerimeter();
 		assertEquals(testPerimeter, myPerimeter);
@@ -25,7 +26,7 @@ public class TestsForQuadrate extends Parent {
 
 	@Parameters("sideOfASquare")
 	@Test(dependsOnMethods = "testGetQuadrate", description="Тестируется правильность вычисления площади")
-	public void testGetSquare(double sideOfASquareFromXML) {
+	public void testGetSquare(@Optional ("") double sideOfASquareFromXML) {
 		double mySquare = Math.pow(sideOfASquareFromXML, 2);
 		double testSquare = getGeomObject().getQuadrate(sideOfASquareFromXML).getSquare();
 		assertEquals(testSquare, mySquare);
